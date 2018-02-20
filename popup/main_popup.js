6d12844223c1d63abe787c1671433f4d1d5cb078
imDema <dema3112@gmail.com>
2018-02-20 23:00:15 +0100

    Initialized script

--- popup/main_popup.js
+++ popup/main_popup.js
@@ -1,15 +1,32 @@
-var dd1 = document.getElementById("first-dropdown");
-var dd2 = document.getElementById("second-dropdown");
-
-for (var i = min; i<=max; i++){
-    addOption(dd1, valore);
-    addOption(dd2, valore);
-}
-
 function addOption(select, option)
 {
     var opt = document.createElement('option');
     opt.value = option;
     opt.innerHTML = option;
-    select.appendChild(select);
+    select.appendChild(opt);
 }
+
+function addToDict(dict, key, value)
+{
+    dict.push({key: key, value: value});
+}
+
+var dd1 = document.getElementById("first-dropdown");
+var dd2 = document.getElementById("second-dropdown");
+
+var dict = [];
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://api.coinmarketcap.com/v1/ticker/", true);
+xhr.setRequestHeader("Content-type", "application/json");
+xhr.send();
+var response = JSON.parse(xhr.responseText);
+
+response.forEach(element => {
+    addToDict(dict, element.id, element.symbol);
+});
+
+dict.forEach(element => {
+    addOption(dd1, element.symbol);
+    addOption(dd2, element.symbol);
+});
