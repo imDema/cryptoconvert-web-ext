404b3c6b48acd4b86e88445c0a851fdfd986064d
imDema <dema3112@gmail.com>
2018-02-24 19:20:54 +0100

    Added fiat

--- popup/main_popup.js
+++ popup/main_popup.js
@@ -11,27 +11,46 @@ function addToDict(dict, key, value)
 }
 //CHANGE EVENTS
 document.addEventListener('DOMContentLoaded',function() {
-    document.querySelector('.dropdown.first').onchange=updateConversion;
+    document.querySelector('.dropdown.first').onchange=updateControls;
 },false);
 document.addEventListener('DOMContentLoaded',function() {
-    document.querySelector('.form-control.first').onchange=updateConversion;
+    document.querySelector('.form-control.first').onchange=updateControls;
 },false);
 document.addEventListener('DOMContentLoaded',function() {
-    document.querySelector('.dropdown.second').onchange=updateConversion;
+    document.querySelector('.dropdown.second').onchange=updateControls;
 },false);
 document.addEventListener('DOMContentLoaded',function() {
-    document.querySelector('.form-control.second').onchange=updateConversion;
+    document.querySelector('.form-control.second').onchange=updateControls;
 },false);
 
-function updateConversion(event)
+const fiat = ["EUR", "USD", "GBP", "KRW"];
+var conversion = 1.0;
+
+function updateControls(event)
 {
-    alert("Change event triggered");
+    switch (event.target.classList) {
+        case ["form-control", "first"]:
+            
+        break;
+    
+        case ["form-control", "second"]:
+        break;
+
+        case ["dropdown", "first"]:
+        break;
+
+        case ["dropdown", "second"]:
+        break;
+
+        default:
+            break;
+    }
 }
 
 // var dict = [];
 
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://api.coinmarketcap.com/v1/ticker/", false);
+xhr.open("GET", "https://api.coinmarketcap.com/v1/ticker/?limit=250", false);
 xhr.setRequestHeader("Content-type", "application/json");
 xhr.send();
 var response = JSON.parse(xhr.responseText);
@@ -40,13 +59,24 @@ var first_recent = document.getElementById("first_recent");
 var first_top = document.getElementById("first_top");
 var second_recent = document.getElementById("second_recent");
 var second_top = document.getElementById("second_top");
+var first_fiat = document.getElementById("first_fiat");
+var second_fiat = document.getElementById("second_fiat");
+
 
 
 // response.forEach(element => {
 //     addToDict(dict, element.id, element.symbol);
 // });
 
+//Populate Top250
 response.forEach(element => {
     addOption(first_top, element.id, element.symbol);
     addOption(second_top, element.id, element.symbol);
+});
+
+//Populate Fiat
+fiat.forEach(element => 
+{
+    addOption(first_fiat, element, element);
+    addOption(second_fiat, element, element);
 });
