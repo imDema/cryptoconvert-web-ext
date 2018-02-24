30f1e43bd09d6e4995203a6034acfadb0aeef522
imDema <dema3112@gmail.com>
2018-02-24 16:09:57 +0100

    Input change events set up

--- popup/main_popup.js
+++ popup/main_popup.js
@@ -5,13 +5,30 @@ function addOption(select, id, symbol)
     opt.innerHTML = symbol;
     select.appendChild(opt);
 }
-
 function addToDict(dict, key, value)
 {
     dict.push({id: key, symbol: value});
 }
+//CHANGE EVENTS
+document.addEventListener('DOMContentLoaded',function() {
+    document.querySelector('.dropdown.first').onchange=updateConversion;
+},false);
+document.addEventListener('DOMContentLoaded',function() {
+    document.querySelector('.form-control.first').onchange=updateConversion;
+},false);
+document.addEventListener('DOMContentLoaded',function() {
+    document.querySelector('.dropdown.second').onchange=updateConversion;
+},false);
+document.addEventListener('DOMContentLoaded',function() {
+    document.querySelector('.form-control.second').onchange=updateConversion;
+},false);
 
-var dict = [];
+function updateConversion(event)
+{
+    alert("Change event triggered");
+}
+
+// var dict = [];
 
 var xhr = new XMLHttpRequest();
 xhr.open("GET", "https://api.coinmarketcap.com/v1/ticker/", false);
@@ -20,16 +37,16 @@ xhr.send();
 var response = JSON.parse(xhr.responseText);
 
 var first_recent = document.getElementById("first_recent");
-var first_alphabetical = document.getElementById("first_alphabetical");
+var first_top = document.getElementById("first_top");
 var second_recent = document.getElementById("second_recent");
-var second_alphabetical = document.getElementById("second_alphabetical");
+var second_top = document.getElementById("second_top");
 
+
+// response.forEach(element => {
+//     addToDict(dict, element.id, element.symbol);
+// });
 
 response.forEach(element => {
-    addToDict(dict, element.id, element.symbol);
-});
-
-response.forEach(element => {
-    addOption(first_alphabetical, element.id, element.symbol);
-    addOption(second_alphabetical, element.id, element.symbol);
+    addOption(first_top, element.id, element.symbol);
+    addOption(second_top, element.id, element.symbol);
 });
